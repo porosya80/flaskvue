4800db2304564fae27240468d4f33503782f346b
Dmytriy Myk <porosya@gmail.com>
2019-06-28 20:26:53 +0300

    finished

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -5,30 +5,31 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Ping',
+  name: "Ping",
   data() {
     return {
-      msg: '',
+      msg: ""
     };
   },
   methods: {
     getMessage() {
-      const path = 'http://localhost:5000/ping';
-      axios.get(path)
-        .then((res) => {
+      const path = "http://localhost:5000/ping";
+      axios
+        .get(path)
+        .then(res => {
           this.msg = res.data;
         })
-        .catch((error) => {
+        .catch(error => {
           // eslint-disable-next-line
           console.error(error);
         });
-    },
+    }
   },
   created() {
     this.getMessage();
-  },
+  }
 };
 </script>
